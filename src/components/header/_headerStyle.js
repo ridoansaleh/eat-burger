9b3e35b0b0c7ba389b0bd5b3f1590c6ef3c2061a
ridoansaleh <ridoansalehnasution98@gmail.com>
2020-05-26 22:29:52 +0700

    add carousel

--- src/components/header/_headerStyle.js
+++ src/components/header/_headerStyle.js
@@ -22,6 +22,7 @@ const useStyles = makeStyles((theme) => ({
   },
   menuButton: {
     marginRight: theme.spacing(2),
+    height: "66px",
   },
   middle: {
     display: "flex",
---
+++ src/routes/home/sections/_carouselStyle.js
@@ -0,0 +1,13 @@
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles(() => ({
+  slider: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    flexDirection: "column",
+    height: "400px",
+  },
+}));
+
+export default useStyles;
