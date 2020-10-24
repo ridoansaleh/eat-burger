c76b6d7d0ba1d9d7282e365b3401910bda26b201
RSN <ridoansalehnasution98@gmail.com>
2020-10-24 20:28:15 +0700

    integrate the app with firebase (#21)

--- src/App.js
+++ src/App.js
@@ -1,84 +1,23 @@
-import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Layout from "./layout";
-import Home from "./routes/home";
-import Menus from "./routes/menus";
-import Order from "./routes/order";
-import OrderSuccess from "./routes/order_success";
-import OrderList from "./routes/order_list";
-import OrderDetail from "./routes/order_detail";
-import ShoppingCart from "./routes/shopping_cart";
-import Registration from "./routes/registration";
-import Profile from "./routes/profile";
-import Login from "./routes/login";
-import {
-  HOME_PATH,
-  MENUS_PATH,
-  ORDER_PATH,
-  ORDER_SUCCESS_PATH,
-  ORDER_LIST_PATH,
-  ORDER_DETAIL,
-  SHOPPING_CART_PATH,
-  REGISTRATION_PATH,
-  PROFILE_PATH,
-  LOGIN_PATH,
-} from "./utils/path";
+import React, { useContext } from "react";
+import { HashRouter, Switch, Route } from "react-router-dom";
+import { FirebaseContext } from "./database";
 
-const app_routes = [
-  {
-    exact: true,
-    path: HOME_PATH,
-    component: Home,
-  },
-  {
-    path: MENUS_PATH,
-    component: Menus,
-  },
-  {
-    path: ORDER_PATH,
-    component: Order,
-  },
-  {
-    path: ORDER_SUCCESS_PATH,
-    component: OrderSuccess,
-  },
-  {
-    path: ORDER_LIST_PATH,
-    component: OrderList,
-  },
-  {
-    path: ORDER_DETAIL,
-    component: OrderDetail,
-  },
-  {
-    path: SHOPPING_CART_PATH,
-    component: ShoppingCart,
-  },
-  {
-    path: REGISTRATION_PATH,
-    component: Registration,
-  },
-  {
-    path: PROFILE_PATH,
-    component: Profile,
-  },
-  {
-    path: LOGIN_PATH,
-    component: Login,
-  },
-];
+import Layout from "./layout";
+import appRoutes from "./routes";
 
 function App() {
+  const context = useContext(FirebaseContext);
+  console.log(context);
   return (
-    <Router>
+    <HashRouter>
       <Layout>
         <Switch>
-          {app_routes.map((data, index) => (
+          {appRoutes.map((data, index) => (
             <Route key={index} {...data} />
           ))}
         </Switch>
       </Layout>
-    </Router>
+    </HashRouter>
   );
 }
 
---
+++ src/routes/index.js
@@ -0,0 +1,68 @@
+import Home from "./home";
+import Menus from "./menus";
+import Order from "./order";
+import OrderSuccess from "./order_success";
+import OrderList from "./order_list";
+import OrderDetail from "./order_detail";
+import ShoppingCart from "./shopping_cart";
+import Registration from "./registration";
+import Profile from "./profile";
+import Login from "./login";
+import {
+  HOME_PATH,
+  MENUS_PATH,
+  ORDER_PATH,
+  ORDER_SUCCESS_PATH,
+  ORDER_LIST_PATH,
+  ORDER_DETAIL,
+  SHOPPING_CART_PATH,
+  REGISTRATION_PATH,
+  PROFILE_PATH,
+  LOGIN_PATH,
+} from "../utils/path";
+
+const app_routes = [
+  {
+    exact: true,
+    path: HOME_PATH,
+    component: Home,
+  },
+  {
+    path: MENUS_PATH,
+    component: Menus,
+  },
+  {
+    path: ORDER_PATH,
+    component: Order,
+  },
+  {
+    path: ORDER_SUCCESS_PATH,
+    component: OrderSuccess,
+  },
+  {
+    path: ORDER_LIST_PATH,
+    component: OrderList,
+  },
+  {
+    path: ORDER_DETAIL,
+    component: OrderDetail,
+  },
+  {
+    path: SHOPPING_CART_PATH,
+    component: ShoppingCart,
+  },
+  {
+    path: REGISTRATION_PATH,
+    component: Registration,
+  },
+  {
+    path: PROFILE_PATH,
+    component: Profile,
+  },
+  {
+    path: LOGIN_PATH,
+    component: Login,
+  },
+];
+
+export default app_routes;
