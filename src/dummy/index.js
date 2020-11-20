6be8de59c97cb74144b7bb923b6098eb786ab6d1
RSN <ridoansalehnasution98@gmail.com>
2020-11-21 06:35:03 +0700

    Improve menus page's style (#23)

---
+++ src/dummy/index.js
@@ -0,0 +1,59 @@
+import beefBurger from "./assets/classic-beef-burger.jpg";
+import elkBurger from "./assets/elk-burger.jpg";
+import portobelloBurger from "./assets/raw-portobello-burger.jpg";
+import turkeyBurger from "./assets/turkey-burger.jpg";
+import veggieBurger from "./assets/veggie-burger.jpg";
+import bisonBurger from "./assets/bison-burgers.jpg";
+import salmonBurger from "./assets/salmon-burger.jpg";
+import blackBeanBurger from "./assets/pumpkin-veggie-burger.jpg";
+
+export const BURGER_LIST = [
+  {
+    name: "Beef Burgers",
+    image: beefBurger,
+    credit: "https://www.eatthis.com/types-of-burgers/",
+    price: 20,
+  },
+  {
+    name: "Elk Burgers",
+    image: elkBurger,
+    credit: "https://www.eatthis.com/types-of-burgers/",
+    price: 15,
+  },
+  {
+    name: "Portobello Mushroom Burgers",
+    image: portobelloBurger,
+    credit: "https://www.eatthis.com/types-of-burgers/",
+    price: 10,
+  },
+  {
+    name: "Turkey Burgers",
+    image: turkeyBurger,
+    credit: "https://www.eatthis.com/types-of-burgers/",
+    price: 14,
+  },
+  {
+    name: "Veggie Burgers",
+    image: veggieBurger,
+    credit: "https://www.eatthis.com/types-of-burgers/",
+    price: 9,
+  },
+  {
+    name: "Bison Burgers",
+    image: bisonBurger,
+    credit: "https://www.eatthis.com/types-of-burgers/",
+    price: 25,
+  },
+  {
+    name: "Wild Salmon Burgers",
+    image: salmonBurger,
+    credit: "https://www.eatthis.com/types-of-burgers/",
+    price: 24,
+  },
+  {
+    name: "Black Bean Burgers",
+    image: blackBeanBurger,
+    credit: "https://www.eatthis.com/types-of-burgers/",
+    price: 17,
+  },
+];
