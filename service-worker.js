b6192facd59cca83dc0c072ef8317da8960e5333
ridoansaleh <ridoansalehnasution98@gmail.com>
2020-11-29 18:13:40 +0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/eat-burger/precache-manifest.7625341d9655848b6e5f0fa93eb485ec.js"
+  "/eat-burger/precache-manifest.e7c9f6d3072dd0e63db05543d8f1196c.js"
 );
 
 self.addEventListener('message', (event) => {
