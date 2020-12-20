ead889e8c543f76016b4673bda7fe449e3d5b3fb
RSN <ridoansalehnasution98@gmail.com>
2020-12-20 18:57:36 +0700

    Add more burgers data and create category in products collection (#46)

---
+++ src/constant/product_category.js
@@ -0,0 +1,28 @@
+const CATEGORY_LIST = [
+  {
+    label: "All",
+    value: "all",
+  },
+  {
+    label: "Latest Event",
+    value: "latest event",
+  },
+  {
+    label: "Promotion",
+    value: "promotion",
+  },
+  {
+    label: "Favorite",
+    value: "favorite",
+  },
+  {
+    label: "New Burger",
+    value: "new burger",
+  },
+  {
+    label: "Best Deals",
+    value: "best deals",
+  },
+];
+
+export default CATEGORY_LIST;
